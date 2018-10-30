9c50f10e559143a1ab8a77925829408a767d8ac6
Chris Minnick <chris@minnick.com>
2018-10-30 06:56:20 -0700

    updated all references to task to thing

---
+++ imports/api/things.js
@@ -0,0 +1,69 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
+
+export const Things = new Mongo.Collection('things');
+
+
+if (Meteor.isServer) {
+    // This code only runs on the server
+    // Only publish things that are public or belong to the current user
+    Meteor.publish('things', function thingsPublication() {
+        return Things.find({
+            $or: [
+                { private: { $ne: true } },
+                { owner: this.userId },
+            ],
+        });
+    });
+}
+
+Meteor.methods({
+    'things.insert'(text) {
+        check(text, String);
+
+        // Make sure the user is logged in before inserting a thing
+        if (! this.userId) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Things.insert({
+            text,
+            createdAt: new Date(),
+            owner: this.userId,
+            username: Meteor.users.findOne(this.userId).username,
+        });
+    },
+    'things.remove'(thingId) {
+        check(thingId, String);
+        const thing = Things.findOne(thingId);
+        if (thing.private && thing.owner !== this.userId) {
+            // If the thing is private, make sure only the owner can delete it
+            throw new Meteor.Error('not-authorized');
+        }
+        Things.remove(thingId);
+    },
+    'things.setChecked'(thingId, setChecked) {
+        check(thingId, String);
+        check(setChecked, Boolean);
+        const thing = Things.findOne(thingId);
+        if (thing.private && thing.owner !== this.userId) {
+            // If the thing is private, make sure only the owner can check it off
+            throw new Meteor.Error('not-authorized');
+        }
+        Things.update(thingId, { $set: { checked: setChecked } });
+    },
+    'things.setPrivate'(thingId, setToPrivate) {
+        check(thingId, String);
+        check(setToPrivate, Boolean);
+
+        const thing = Things.findOne(thingId);
+
+        // Make sure only the thing owner can make a thing private
+        if (thing.owner !== this.userId) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Things.update(thingId, { $set: { private: setToPrivate } });
+    },
+});
