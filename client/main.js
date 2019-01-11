f9befa3c9eaa56ab028617df65be51d51e649bd8
Chris Minnick <chris@minnick.com>
2019-01-11 14:46:03 -0800

    got rid of errors

--- client/main.js
+++ client/main.js
@@ -8,12 +8,12 @@ import App from '../imports/ui/App.vue';
 import { Things } from '../imports/api/things.js';
 
 Meteor.startup(() => {
-    const text = "test text";
-    const quantity = 5;
+    //const text = "test text";
+    //const quantity = 5;
 
-    Meteor.call('things.insert', text, quantity);
-    let things = Things.find({}, { sort: { createdAt: -1 } }).fetch();
-    console.log(JSON.stringify(things));
+    //Meteor.call('things.insert', text, quantity);
+    //let things = Things.find({}, { sort: { createdAt: -1 } }).fetch();
+    //console.log(JSON.stringify(things));
 
     new Vue({
         render: function (h) { return h(App) },
